3014a7c16fe13867fa5889c80f256a806be3b03a
Adityamaharia <adityamaharia229@gmail.com>
2025-08-15 06:45:07 -0700

    comments added to js file

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const display = document.getElementById("display");
 
+// Append a value (number or operator) to the display
 function appendToDisplay(value) {
   display.value += value;
 }
 
+// Clear the display (reset to "0")
 function clearDisplay() {
   display.value = "0";
 }
 
+// Add a decimal point only if the current number chunk doesn’t already have one
 function addDot(dot) {
   let currentValue = display.value;
 
-  // Find the last "number chunk" after the last operator
+  // Get the last number after the most recent operator
   let lastNumber = currentValue.split(/[\+\-\*\/]/).pop();
 
-  // If last number doesn't have a dot, allow adding one
+  // Only add dot if this number chunk doesn't have one yet
   if (!lastNumber.includes(dot)) {
-    // If display is empty or last char is operator, start with 0.
+    // If empty or last char is an operator, start with "0."
     if (currentValue === "" || /[\+\-\*\/]$/.test(currentValue)) {
       display.value += "0" + dot;
     } else {
@@ -25,6 +28,7 @@ function addDot(dot) {
   }
 }
 
+// Calculate and display the result
 function calculateResult() {
   if (display.value === "" || display.value === "Error") {
     display.value = "";
@@ -37,6 +41,7 @@ function calculateResult() {
   }
 }
 
+// Calculate square root of the displayed number
 function squareRoot() {
   let num = display.value;
   if (num === "error") {
@@ -48,6 +53,7 @@ function squareRoot() {
   }
 }
 
+// Convert number to percentage
 function percentage() {
   let num = display.value;
   if (num === "error") {
@@ -59,10 +65,12 @@ function percentage() {
   }
 }
 
+// Handle backspace to remove the last character
 function backspace() {
   display.value = display.value.slice(0, -1);
 }
 
+// Change the sign of the current number (positive to negative or vice versa)
 function changeSign() {
   let num = display.value;
   if (num === "" || isNaN(num)) {
@@ -72,6 +80,7 @@ function changeSign() {
   }
 }
 
+// Calculate the square of the displayed number
 function square() {
   let num = display.value;
   if (num === "error") {
